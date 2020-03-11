75cd869235e6e9f7822c7878a3a831ab51de8bc6
jang hei jung <heijung.jang@hotmail.com>
2020-03-11 19:13:32 +0900

    9번 퀴즈

---
+++ 장혜정/quiz09.py
@@ -0,0 +1,25 @@
+"""
+9. 점수 구간에 해당하는 학점이 아래와 같이 정의되어 있다.
+점수를 입력했을 때 해당 학점이 출력되도록 하시오.
+81~100 : A
+61~80 : B
+41~60 : C
+21~40 : D
+0~20 : F
+"""
+def my_grade(score):
+    if (score>80) and (score<=100):
+        grade = 'A'
+    elif (score>60) and (score<=80):
+        grade = 'B'
+    elif (score>40) and (score<=60):
+        grade = 'C'
+    elif (score>20) and (score<=40):
+        grade = 'D'
+    elif (score>=0) and (score<=20):
+        grade = 'F'
+    return grade
+
+score = int(input('점수를 입력하시오 : '))
+grade = my_grade(score)
+print('학점은 {} 입니다.'.format(grade))
