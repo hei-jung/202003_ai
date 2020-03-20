58f4d566452383c5a1524aa8a497bbd8635440d3
jang hei jung <heijung.jang@hotmail.com>
2020-03-20 19:33:14 +0900

    a

--- 장혜정/quiz08.py
+++
@@ -1,9 +0,0 @@
-"""8. 정수를 입력했을 때 짝수인지 홀수인지 핀별하는 코드를 작성하시오"""
-integer = int(input('정수를 입력하시오 : '))
-
-if integer == 0:
-    print('0입니다.')
-elif integer%2 == 0:
-    print('짝수입니다.')
-else:
-    print('홀수입니다.')
--- 장혜정/quiz19.py
+++
@@ -1,7 +0,0 @@
-"""19. 다음 리스트에서 알파벳 개수가 7개인 단어를 출력하시오
-a = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india']
- """
-a = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india']
-for i in range(len(a)):
-    if len(a[i]) == 7:
-        print(a[i])
